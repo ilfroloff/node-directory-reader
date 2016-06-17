8a0a74ff6a45673f6741097cfb3cf0a8d9d16f33
Ilya Frolov <ilfroloff@gmail.com>
2016-06-17 16:32:28 +0300

    v0.1.0 release notes:
    Changes:
    - added "level" option: set level of searching
    - added "directories" option: includes or not directory path to results
    - changes "mask" option:
    -- instead of file name masks content path
    -- can be applied to directory path

--- index.js
+++ index.js
@@ -1,47 +1,129 @@
-var diread = function(options) {
-    var dir_path = options.src,
-        mask = typeof options.mask === 'function'? options.mask : function() { return true; },
-        execute_dir = process.cwd();
+'use strict';
 
-    var fs = require('fs'),
-        path = require('path'),
-        full_dir_path = path.resolve(execute_dir, dir_path),
-        list_of_file_path = [],
+var FS = require('fs');
+var Path = require('path');
 
-        read_by_path = function(dir_path, file_name) {
-            var file_path = path.join(dir_path, file_name),
-                is_directory = fs.statSync(file_path).isDirectory();
-
-            if(is_directory) {
-                read_directory(file_path);
-            } else if(mask(file_name)) {
-                list_of_file_path.push(file_path);
-            }
-        },
-        read_directory = function(dir_path) {
-            var files = fs.readdirSync(dir_path);
-
-            files.forEach(function(file_name) {
-                read_by_path(dir_path, file_name);
-            });
-        };
-
-    try {
-        read_by_path(full_dir_path, '');
-    } catch(err) {
+/**
+ *
+ * @class
+ * @param options
+ * @returns {Diread}
+ */
+function Diread(options) {
+    if(!(this instanceof Diread)) {
+        return new Diread(options);
     }
 
-    return {
-        list: function(callback) {
-            return callback(list_of_file_path);
-        },
-        each: function(callback) {
-            return list_of_file_path.forEach(callback);
-        },
-        map: function(callback) {
-            return list_of_file_path.map(callback);
-        }
-    };
+    this.initialize(options);
+}
+
+/**
+ * Creates directory reader
+ *
+ * @param {Object}    options
+ * @param {String}    options.src
+ * @param {Boolean}  [options.directories=false]
+ * @param {Number}   [options.level]
+ * @param {Function} [options.mask]
+ */
+Diread.prototype.initialize = function(options) {
+    this._directory_path = Path.resolve(process.cwd(), options.src);
+    this._level = options.level || -1;
+    this._check_directories = options.directories || false;
+    this._mask = typeof options.mask === 'function'?
+        options.mask :
+        function() { return true; };
+
+    this._current_read_level = 0;
+    this._list_of_file_path = [];
+
+    try {
+        this._read_by_path(this._directory_path, '');
+    } catch(err) {
+    }
 };
 
-module.exports = diread;
+/**
+ * Returns list of find paths
+ *
+ * @param {Function} callback
+ */
+Diread.prototype.list = function(callback) {
+    callback(this._list_of_file_path);
+};
+
+/**
+ * Iterates by all paths
+ *
+ * @param {Function} callback
+ */
+Diread.prototype.each = function(callback) {
+    this._list_of_file_path.forEach(callback);
+};
+
+/**
+ * Maps by all paths
+ *
+ * @param {Function} callback
+ * @returns {Array}
+ */
+Diread.prototype.map = function(callback) {
+    return this._list_of_file_path.map(callback);
+};
+
+/**
+ * Reads by directory path and filters by directory path or/and file name
+ *
+ * @param {String} directory_path
+ * @param {String} [filename]
+ */
+Diread.prototype._read_by_path = function(directory_path, filename) {
+    var path = Path.join(directory_path, filename);
+    var is_directory = FS.statSync(path).isDirectory();
+
+    if(is_directory) {
+        if(this._check_directories) {
+            if(!this._mask(path)) {
+                return;
+            }
+
+            if(path !== this._directory_path) {
+                this._list_of_file_path.push(path);
+            }
+        }
+
+        this._current_read_level++;
+        this._read_directory(path);
+
+        return;
+    }
+
+    if(this._mask(path, filename)) {
+        this._list_of_file_path.push(path);
+    }
+};
+
+/**
+ * Reads and iterates directory content
+ *
+ * @param {String} directory_path
+ */
+Diread.prototype._read_directory = function(directory_path) {
+    if(this._level !== -1) {
+        if(this._current_read_level > this._level) {
+            this._current_read_level--;
+            return;
+        }
+    }
+
+    var self = this;
+    var files = FS.readdirSync(directory_path);
+
+    files.forEach(function(content_name) {
+        self._read_by_path(directory_path, content_name);
+    });
+
+    this._current_read_level--;
+};
+
+module.exports = Diread;
--- test/test.js
+++ test/test.js
@@ -1,33 +1,63 @@
-var diread = require('../index.js');
+'use strict';
+
+var Diread = require('../index.js');
+var Path = require('path');
 var assert = require('assert');
 
 describe('Check reading folder files', function() {
     it('should return 2 file names', function() {
-        diread({ src: 'test/folder' }).list(function(list) {
-            assert.equal(2, list.length);
+        Diread({ src: 'test/folder' }).list(function(list) {
+            assert.equal(3, list.length);
         });
     });
 
-    it('should find file with name file2.js', function() {
-        diread({
+    it('should find file with name file1-2.js', function() {
+        Diread({
             src: 'test/folder',
-            mask: function(file_name) {
-                return file_name.indexOf('file2.js') !== -1;
+            mask: function(path) {
+                return path.indexOf('file1-2.js') !== -1;
             }
         }).list(function(list) {
             assert.equal(1, list.length);
         });
     });
 
-    it('should return fullpath and file name', function() {
-        diread({
+    it('should return file path', function() {
+        Diread({
             src: 'test/folder/folder',
-            mask: function(file_name) {
-                return file_name.indexOf('file2.js') !== -1;
+            mask: function(path) {
+                return path.indexOf('file1') !== -1;
             }
-        }).each(function(full_path, file_name) {
-            assert.equal(full_path, list.length);
-            assert.equal('file2.js', file_name);
+        }).each(function(path) {
+            assert.equal('file2-1.js', Path.basename(path));
+        });
+    });
+
+    it('should return paths to all items', function() {
+        Diread({
+            src: 'test/folder',
+            directories: true
+        }).list(function(list) {
+            assert.equal(4, list.length);
+        });
+    });
+
+    it('should return files from first level', function() {
+        Diread({
+            src: 'test/folder',
+            level: 1
+        }).list(function(list) {
+            assert.equal(2, list.length);
+        });
+    });
+
+    it('should return files and directories from first level', function() {
+        Diread({
+            src: 'test/folder',
+            directories: true,
+            level: 1
+        }).list(function(list) {
+            assert.equal(3, list.length);
         });
     });
 });
